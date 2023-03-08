b7f2b58f4fa0a0e0a11d3f44fdd50cd22b59622a
kdan2944 <kdan2944gmail.com>
2023-03-08 13:27:10 +0300

    fix linter error

--- src/__test__/components/Missions.test.js
+++ src/__test__/components/Missions.test.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import Missions from '../../components/Missions';
 import store from '../../redux/store';
+
 describe('Missions', () => {
   it('renders correctly for reserved state', () => {
     const tree = renderer
--- src/__test__/components/Navbar.test.js
+++ src/__test__/components/Navbar.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import Navbar from '../../components/Navbar'
+import Navbar from '../../components/Navbar';
+
 describe('Navbar', () => {
   it('renders correctly', () => {
     const tree = renderer
